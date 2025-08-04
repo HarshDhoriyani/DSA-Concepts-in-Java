0ee3ff5ff3fc92c4ec40d3db60724d6660dadb67
Harsh Dhoriyani <harshdhoriyani2005@gmail.com>
2025-08-04 19:33:37 +0530

    LinkedLists Part2

---
+++ LinkedLists/ZigZagLL.java
@@ -0,0 +1,96 @@
+
+public class ZigZagLL {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    
+
+
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public void zigzag() {
+        // 1. find middle node of the linked list
+        Node midNode = findMid(head);
+
+        // 2. Reverse the second half of the linked list
+        Node curr = midNode.next;
+        midNode.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        // 3. Alternate merging
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            right = nextR;
+            left = nextL;
+        }
+    }
+    
+    public static void main(String[] args) {
+        ZigZagLL ll = new ZigZagLL();
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+
+        ll.display();
+        ll.zigzag();
+        ll.display();
+
+
+    }
+}
